82ae50d355834cf79f4586392da58c60f82fce39
emas130 <pepe130he@wp.pl>
2018-02-22 13:15:28 +0100

    init

--- app/program.js
+++ app/program.js
@@ -1,8 +1,12 @@
+var Osinfo = require('../modules/OSinfo');
+var os = require('os');
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
 	var input = process.stdin.read();
 	if (input !== null) {
 		var instruction = input.trim();
+
 		switch (instruction) {
 			case '/exit':
 				process.stdout.write('Quitting app!\n');
@@ -11,8 +15,15 @@ process.stdin.on('readable', function () {
 			case '/sayhello':
 				process.stdout.write('hello!\n');
 				break;
+
+			case '/getOSinfo':
+				getOSinfo();
+
+				break;
 			default:
 				process.stderr.write('Wrong instruction!\n');
 		};
 	}
 });
+
+Osinfo.print();
