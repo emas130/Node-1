d6b20c142d298e4331b8052f35b49201555aa1be
emas130 <34860891+emas130@users.noreply.github.com>
2018-03-05 20:10:08 +0100

    Update Time.js

--- modules/Time.js
+++ modules/Time.js
@@ -1,11 +1,11 @@
-var os = require('os');
-process.stdin.setEncoding('utf-8');
+function getFormatTime(time) {
+    var hours = Math.floor(time / 3600);
+    time %= 3600;
 
-function time () {
-var uptime = os.uptime();
-console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    var minutes = Math.floor(time / 60);
+    var seconds = (time % 60).toFixed(0);
+
+    return hours + " hours " + minutes + " minutes " + seconds + " seconds.";
 }
 
-console.log(time());
-exports.print = time;
- 
+exports.print = getFormatTime;
