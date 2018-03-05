f53bf855887b333d5d05e6d9bc04f0ecdf54543f
emas130 <34860891+emas130@users.noreply.github.com>
2018-03-05 20:08:00 +0100

    Update program.js

--- app/program.js
+++ app/program.js
@@ -1,30 +1,27 @@
-var Osinfo = require('../modules/OSinfo');
-var time = require('../modules/Time');
+var Osinfo = require('os');
+var time = require('../modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function () {
 	var input = process.stdin.read();
 	if (input !== null) {
 		var instruction = input.trim();
 
 		switch (instruction) {
-			case '/exit':
-				process.stdout.write('Quitting app!\n');
+			case "/exit":
+				process.stdout.write("Quitting app!\n");
 				process.exit();
 				break;
-			case '/sayhello':
-				process.stdout.write('hello!\n');
+			case "/version":
+				process.stdout.write("Node version is: " + process.versions.node + "\n");
 				break;
-
-			case '/getOSinfo':
-				getOSinfo();
+			case "/getOSinfo":
+				OSinfo.print();
 				break;
-				
 			default:
-				process.stderr.write('Wrong instruction!\n');
+				process.stderr.write("Wrong instruction!\n");
+				break;
 		};
 	}
 });
-
-Osinfo.print();
-time.print();
